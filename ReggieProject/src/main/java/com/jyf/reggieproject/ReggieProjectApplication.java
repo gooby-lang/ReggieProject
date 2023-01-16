ff328a5388cdc9115e9082d29990a17f3596b687
gooby-lang <1767818223@qq.com>
2023-01-16 15:41:26 +0800

    first commit

---
+++ ReggieProject/src/main/java/com/jyf/reggieproject/ReggieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jyf.reggieproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReggieProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieProjectApplication.class, args);
+    }
+
+}
---
+++ ReggieProject/src/test/java/com/jyf/reggieproject/ReggieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyf.reggieproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReggieProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
